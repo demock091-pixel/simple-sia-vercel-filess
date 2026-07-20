a1bb393c430473416db24c8c90610743c72302d7
demo <demock091@gmail.com>
2026-07-20 21:51:02 +0700

    cihuyyyy

--- config.py
+++ config.py
@@ -1,17 +1,20 @@
+import os
 class Config:
     """
     Konfigurasi aplikasi Flask
     """
 
-    # Konfigurasi Database MySQL
-    HOST = "localhost"
-    PORT = 3306
-    USER = "root"
-    PASSWORD = "root"
-    DATABASE = "db_akademik"
+  # Konfigurasi Database MySQL
+    HOST = os.environ.get("DB_HOST", "localhost")
+    PORT = int(os.environ.get("DB_PORT", 3306))
+    USER = os.environ.get("DB_USER", "root")
+    PASSWORD = os.environ.get("DB_PASSWORD", "root")
+    DATABASE = os.environ.get("DB_NAME", "db_akademik")
 
     # Secret Key Flask
-    SECRET_KEY = "praktikum-flask-2026"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "praktikum-flask-2026")
 
     # Konfigurasi Flask
-    DEBUG = True
+    # Diubah default ke False -- DEBUG=True di production membocorkan
+    # traceback lengkap (termasuk kredensial DB) ke publik kalau error.
+    DEBUG = os.environ.get("FLASK_DEBUG", "False") == "True"
